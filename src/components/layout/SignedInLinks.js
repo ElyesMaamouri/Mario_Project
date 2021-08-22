cec6aca5cc849048357ffb77334b0845a5a6e782
ElyesMaamouri <elyesmaamouri@gmail.com>
2021-08-22 16:11:03 +0100

    Add SignedInLinks Component

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        {/* class Active react */}
+        <NavLink to="/">New Project</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Log Out</NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating pink lighten-1">
+          EM
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
