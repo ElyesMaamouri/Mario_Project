dcf2c520321612c5ed82d44a283402d1d7330950
ElyesMaamouri <elyesmaamouri@gmail.com>
2021-08-25 14:39:21 +0100

    add new config firebase && config redux with firebase and firestore

--- src/components/store/actions/projectActions.js
+++ src/components/store/actions/projectActions.js
@@ -1,5 +1,6 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  //getFirebase & getFirestore pour connecter au firebase
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // call api
     dispatch({ type: "CREATE_PROJECT", project: project });
   };
--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -2,17 +2,16 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCUu1sPWbA1xJHDPO4sNLVMbuu0aPxp3pg",
-  authDomain: "marioproject-99b85.firebaseapp.com",
-  projectId: "marioproject-99b85",
-  storageBucket: "marioproject-99b85.appspot.com",
-  messagingSenderId: "272907060662",
-  appId: "1:272907060662:web:c92eeda50159e430496d17",
-  measurementId: "G-17E20RV4DR",
+var firebaseConfig = {
+  apiKey: "AIzaSyBLayFZV5d5IIUxahtSe3pq8clPXbaGP14",
+  authDomain: "mario-be968.firebaseapp.com",
+  projectId: "mario-be968",
+  storageBucket: "mario-be968.appspot.com",
+  messagingSenderId: "832658702917",
+  appId: "1:832658702917:web:84947819a41f82da39192c",
 };
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore().settings({ timestampsInSnapshots: true });
 
-export default firebase;
+export default firebaseConfig;
